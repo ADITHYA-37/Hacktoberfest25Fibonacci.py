8c1858543b8a361f081c66d931d82eddb69c1252
ADITHYA-37 <adithyasurisetti9@gmail.com>
2025-10-27 11:18:29 +0530

    Update Fibonacci.py
    Changed the Fibonacci code to prompt the user for the number of terms and print that many numbers from the sequence, rather than just returning the nth Fibonacci number. This makes the script interactive by displaying the entire Fibonacci sequence up to the user’s chosen length.
    
    Please consider this code.

--- Fibonacci.py
+++ Fibonacci.py
@@ -1,11 +1,18 @@
-def fibonacci(n):
-    if n <= 1:
-        return n
-    dp = [0] * (n + 1)
+def fibonacci_sequence(num_terms):
+    if num_terms <= 0:
+        return []
+    elif num_terms == 1:
+        return [0]
+    # Create dp list to hold all Fibonacci numbers up to num_terms
+    dp = [0] * num_terms
     dp[0] = 0
     dp[1] = 1
-    for i in range(2, n + 1):
+    for i in range(2, num_terms):
         dp[i] = dp[i - 1] + dp[i - 2]
-    return dp[n]
+    return dp
 
-print(fibonacci(6))  # ✅ Output: 8
+# Take input from user
+n = int(input("Enter the number of Fibonacci terms to print: "))
+fib_seq = fibonacci_sequence(n)
+print("Fibonacci sequence:")
+print(' '.join(str(num) for num in fib_seq))
